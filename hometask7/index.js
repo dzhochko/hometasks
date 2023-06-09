c277ef1781d59ecc7cd85f8bc9e326b363f16112
dzhochko <101223408+dzhochko@users.noreply.github.com>
2023-06-09 15:33:57 +0300

    Add files via upload

---
+++ hometask7/index.js
@@ -0,0 +1,107 @@
+// TASK 1 ------
+
+// const myWindow = window.open("https://www.google.com/", "", "width=300,height=300");
+
+// setTimeout(function () {
+// 	myWindow.resizeTo(500, 500);
+// }, 2000);
+
+// setTimeout(function () {
+// 	myWindow.moveTo(200, 200);
+// }, 4000);
+
+// setTimeout(function () {
+// 	myWindow.close();
+// }, 6000);
+
+// TASK 1 ------
+
+// TASK 2 ------
+
+// function changeCSS() {
+// 	const paragraph = document.getElementById('text');
+
+// 	paragraph.style.color = 'orange';
+// 	paragraph.style.fontSize = '20px';
+// 	paragraph.style.fontFamily = 'Comic Sans MS';
+// }
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', changeCSS);
+
+// TASK 2 ------
+
+
+// TASK 3 ------
+
+// const button1 = document.getElementById('button1');
+// const button2 = document.getElementById('button2');
+// const button3 = document.getElementById('button3');
+// const link = document.getElementById('link');
+
+// button1.addEventListener('click', function () {
+// 	document.body.style.backgroundColor = 'blue';
+// });
+
+// button2.addEventListener('dblclick', function () {
+// 	document.body.style.backgroundColor = 'pink';
+// });
+
+// button3.addEventListener('mousedown', function () {
+// 	document.body.style.backgroundColor = 'brown';
+// });
+
+// button3.addEventListener('mouseup', function () {
+// 	document.body.style.backgroundColor = 'white';
+// });
+
+// link.addEventListener('mouseenter', function () {
+// 	document.body.style.backgroundColor = 'yellow';
+// });
+
+// link.addEventListener('mouseleave', function () {
+// 	document.body.style.backgroundColor = 'white';
+// });
+
+// TASK 3 ------
+
+
+
+// TASK 4 ------
+
+// const dropdownList = document.getElementById('dropdownList');
+// const deleteButton = document.getElementById('deleteButton');
+
+// deleteButton.addEventListener('click', function () {
+// 	const selectedIndex = dropdownList.selectedIndex;
+
+// 	if (selectedIndex !== -1) {
+// 		dropdownList.remove(selectedIndex);
+// 	}
+// });
+
+// TASK 4 ------
+
+
+// TASK 5 ------
+
+// const myButton = document.getElementById('myButton');
+// const message = document.getElementById('message');
+
+// myButton.addEventListener('click', function () {
+// 	message.textContent = 'I was pressed!';
+// });
+
+// myButton.addEventListener('mouseover', function () {
+// 	message.textContent = 'Mouse on me!';
+// });
+
+// myButton.addEventListener('mouseout', function () {
+// 	message.textContent = 'Mouse is not on me!';
+// });
+
+// TASK 5 ------
+
+
+
