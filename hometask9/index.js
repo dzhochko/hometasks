e46bc797596a204780a892dd78255b3fd3efd121
dzhochko <101223408+dzhochko@users.noreply.github.com>
2023-06-16 16:54:10 +0300

    Add files via upload

---
+++ hometask9/index.js
@@ -0,0 +1,37 @@
+// TASK 1 -------------
+
+// $(document).ready(function () {
+// 	$('h2.head').each(function () {
+// 		$(this).css('background-color', 'green');
+
+// 		$(this).find('.inner').each(function () {
+// 			$(this).css('font-size', '35px');
+// 		});
+// 	});
+// });
+
+// TASK 1 -------------
+
+
+// TASK 2 -------------
+
+// $(document).ready(function () {
+// 	$('a[href^="https://"]').each(function () {
+// 		$(this).attr('target', '_blank');
+// 	});
+// });
+
+// TASK 2 -------------
+
+
+// TASK 3 -------------
+
+// $(document).ready(function () {
+// 	$('h3').each(function () {
+// 		const nextDiv = $(this).next('div');
+
+// 		$(this).before(nextDiv);
+// 	});
+// });
+
+// TASK 3 -------------
